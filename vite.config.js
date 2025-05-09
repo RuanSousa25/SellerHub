f2127b97d9b05444975acc48de743da48589bccd
Ruan Sousa <ruansousad4c@gmail.com>
2025-05-09 08:30:52 -0300

    correção do app para deploy no vercel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/SellerHub",
-  plugin: [react()],
-  server: {
-    historyApiFallback: true,
-  },
+  base: "/SellerHub/",
+  plugins: [react()],
 });
